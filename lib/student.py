4c0a9df5716d2429938baedcac4c38a62980882b
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-27 12:30:55 +0300

    Mission Passed

--- lib/student.py
+++ lib/student.py
@@ -1,34 +1,44 @@
 class Student:
-    def __init__(self, hello, raise_hand):
-        self._hello = hello
-        self._raise_hand = raise_hand
+     def __init__(self):
+         self._hello = "Hey there! I'm so excited to learn stuff."
+         self._raise_hand = "Pick me!"
 
-    def hello(self, hello):
-        if hello == "":
-            print("Hey there! I'm so excited to learn stuff.")
-        else:
+     def set_hello(self, hello):
+         if hello == "":
+            self._hello = "Hey there everyone!"
+            return self._hello
+         else:
             self._hello = hello
-        
+            return None
+   
+     def hello(self):
+        return self._hello
     
-    def raise_hand(self):
-        attention_seeker = "Pick me!"
-        print(attention_seeker)
-        return attention_seeker
-        
+     def raise_hand(self):
+       return self._raise_hand
+  
         
 class ChattyStudent(Student):
-    def __init__(self, hello, raise_hand):
-        super().__init__(hello, raise_hand)
+     def __init__(self):
+        super().__init__()
     
-    def hello(self, hello):
-        super()._hello(hello)
-        hello = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
-        print(hello)
-        return hello
-    
-    def raise_hand(self):
+     def set_hello(self, hello):
+        super().set_hello(hello)
+        self._hello = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+        return self._hello
+     
+     def raise_hand(self):
         chatty_phrase = super().raise_hand()
         annoying_phrase = chatty_phrase * 10
         print (annoying_phrase)
         return annoying_phrase
+    
+student = Student()
+print(student.hello())
+print(student.raise_hand())
+print(student.set_hello(""))
+
+chatty_student =  ChattyStudent()
+# print(chatty_student.set_hello())
+print(chatty_student.raise_hand())
         
