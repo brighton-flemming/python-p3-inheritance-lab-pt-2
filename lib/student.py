e1433f3f6956c6eba2f5f98727cbf7326c4144fd
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-27 12:42:09 +0300

    Mission Passed

--- lib/student.py
+++ lib/student.py
@@ -21,17 +21,23 @@ class Student:
 class ChattyStudent(Student):
      def __init__(self):
         super().__init__()
+        self._hello = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
     
      def set_hello(self, hello):
         super().set_hello(hello)
-        self._hello = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+        self._hello = "Hey there! How's your day going? If you need a chat, some info, or just want to share something cool, I'm all ears (or, well, text!)."
         return self._hello
+     
+     def hello(self):
+        return self._hello
+     
      
      def raise_hand(self):
         chatty_phrase = super().raise_hand()
         annoying_phrase = chatty_phrase * 10
         print (annoying_phrase)
         return annoying_phrase
+     
     
 student = Student()
 print(student.hello())
@@ -39,6 +45,7 @@ print(student.raise_hand())
 print(student.set_hello(""))
 
 chatty_student =  ChattyStudent()
-# print(chatty_student.set_hello())
+print(chatty_student.hello())
 print(chatty_student.raise_hand())
+print(chatty_student.set_hello("Hello everyone! I'm really chatty!"))
         
--- lib/testing/student_test.py
+++ lib/testing/student_test.py
@@ -1,49 +1,39 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 
-from student import Student, ChattyStudent
 
-import io
-import sys
+import unittest
+from student import Student, ChattyStudent  # Import your classes from student.py
 
-class TestStudent:
-    '''Class Student in student.py'''
-    
-    def test_says_hello(self):
-        '''says a brief greeting.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+class TestStudent(unittest.TestCase):
+    def test_default_hello(self):
         student = Student()
-        student.hello()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n")
+        self.assertEqual(student.hello(), "Hey there! I'm so excited to learn stuff.")
 
-    def test_raises_hand(self):
-        '''respectfully tries to get the teacher's attention.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+    def test_set_hello(self):
         student = Student()
-        student.raise_hand()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Pick me!\n")
+        self.assertIsNone(student.set_hello("Hello, world!"))
+        self.assertEqual(student.hello(), "Hello, world!")
 
-class TestChattyStudent:
-    '''Class ChattyStudent in student.py'''
-    
-    def test_says_hello(self):
-        '''says a brief greeting, then tries to spoil a TV show.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        chatty_student = ChattyStudent()
-        chatty_student.hello()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n" +
-            "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n")
+    def test_raise_hand(self):
+        student = Student()
+        self.assertEqual(student.raise_hand(), "Pick me!")
 
-    def test_raises_hand(self):
-        '''respectfully tries to get the teacher's attention ten times.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+class TestChattyStudent(unittest.TestCase):
+    def test_default_hello(self):
         chatty_student = ChattyStudent()
-        chatty_student.raise_hand()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n")
+        expected = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+        self.assertEqual(chatty_student.hello(), expected)
+
+    def test_set_hello(self):
+        chatty_student = ChattyStudent()
+        expected = "Hey there! How's your day going? If you need a chat, some info, or just want to share something cool, I'm all ears (or, well, text!)."
+        self.assertEqual(chatty_student.set_hello("Hello, everyone!"), expected)
+        self.assertEqual(chatty_student.hello(), expected)
+
+    def test_raise_hand(self):
+        chatty_student = ChattyStudent()
+        expected = "Pick me!" * 10
+        self.assertEqual(chatty_student.raise_hand(), expected)
+
+if __name__ == '__main__':
+    unittest.main()
